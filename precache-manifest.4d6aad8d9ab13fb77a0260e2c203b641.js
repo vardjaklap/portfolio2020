d4d9016f2cf0a4cc3c31df08653f408d90de48cc
vardjaklap <alexperesta@gmail.com>
2020-02-12 20:11:41 -0500

    Updates

---
+++ precache-manifest.4d6aad8d9ab13fb77a0260e2c203b641.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa90f7a0d93506bd87a2ff0e513eb56a",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "128f96bc09c08b0c1cd4",
+    "url": "/portfolio2020/static/css/main.ec5d6dbf.chunk.css"
+  },
+  {
+    "revision": "60b9872eafa7ee333fed",
+    "url": "/portfolio2020/static/js/2.4f1f9cb0.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/portfolio2020/static/js/2.4f1f9cb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "128f96bc09c08b0c1cd4",
+    "url": "/portfolio2020/static/js/main.a4cddc9c.chunk.js"
+  },
+  {
+    "revision": "20208710cd0d2fd76293",
+    "url": "/portfolio2020/static/js/runtime-main.1c7bf1ae.js"
+  },
+  {
+    "revision": "59598323828525c9c332ff43e44e6a7c",
+    "url": "/portfolio2020/static/media/avatar.59598323.png"
+  },
+  {
+    "revision": "5354a5d0b9cafc38549d9ac6bee78ef0",
+    "url": "/portfolio2020/static/media/dec1.5354a5d0.png"
+  },
+  {
+    "revision": "7e50cd5ec17ad9780262d25dbcca0f6e",
+    "url": "/portfolio2020/static/media/dec2.7e50cd5e.png"
+  }
+]);
